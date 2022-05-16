ee678ae39737c59956f253491334889cf4f2db7c
Alap Mistry <alapmistry23@gmail.com>
2022-05-16 20:18:32 +0530

    Initial commit

---
+++ src/main/java/com/example/DemoKubesphereApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKubesphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKubesphereApplication.class, args);
+	}
+
+}
